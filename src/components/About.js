1183ca0f58a1af6a59b1a188bb031cf0ede2b473
devbackend046 <devbackend046@gmail.com>
2025-03-13 14:43:48 +0530

    modified about section

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export function About() {
   return (
     <div
       name="about"
-      style={{backgroundColor: 'rgb(18,15,22)'}}
+      style={{ backgroundColor: "rgb(18,15,22)" }}
       className="md:h-screen w-full   text-white"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
@@ -15,18 +15,31 @@ export function About() {
           </p>
         </div>
 
-        <p className="mt-16 text-2xl font-extrabold text-zinc-200"> Vijay Jawalkar</p>
-        <span className=" w-[160px] mb-8 my-2  px-2 py-1 text-sm bg-zinc-900 font-semibold text-emerald-500" >Full Stack Developer</span> 
+        <p className="mt-16 text-2xl font-extrabold text-zinc-200">
+          {" "}
+          Vijay Jawalkar
+        </p>
+        <span className=" w-[160px] mb-8 my-2  px-2 py-1 text-sm bg-zinc-900 font-semibold text-emerald-500">
+          Full Stack Developer
+        </span>
 
         <p className="text-xl ">
-        Hello! I'm a full-stack developer from Betul district, currently living and working in Bhopal as a MERN stack developer intern at Zappian. I have a passion for creating web applications that address real-world problems. My journey began with learning frontend technologies like ReactJS, and I later expanded my skills to include backend development like nodejs, express and mongodb.
+          Hello! I'm a full-stack developer with over a year of experience from
+          Betul district, currently living and working in Pune as a Full-Stack
+          Developer at Custom Travel Solutions. I have a passion for building
+          web applications that solve real-world problems. My journey started
+          with frontend technologies like ReactJS, and I later expanded into
+          backend development with Node.js, Express, and MongoDB. Currently, I'm
+          contributing to full-stack development on a product.
         </p>
 
         <br />
 
         <p className="text-xl">
-     
-I am a quick learner with a constant curiosity for new technologies that can help solve problems. As a good team player, I strive to build strong relationships with everyone. In my free time, I enjoy watching movies and listening to audiobooks.
+          I am a quick learner with a constant curiosity for new technologies
+          that can help solve problems. As a good team player, I strive to build
+          strong relationships with everyone. In my free time, I enjoy watching
+          movies and listening to audiobooks.
         </p>
       </div>
     </div>
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,45 +1,114 @@
-
-
 export function Experience() {
-    return (
-        <div
-        name="experience"
-        style={{backgroundColor: 'rgb(18,15,22)', minHeight: '100vh'}}
-        className="w-full text-white"
+  return (
+    <div
+      name="experience"
+      style={{ backgroundColor: "rgb(18,15,22)", minHeight: "100vh" }}
+      className="w-full text-white"
     >
-        <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
-            <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Experience</p>
-                <p className='py-6'>These are the organizations I have worked with</p>
-            </div>
-            <ol className="relative border-l border-gray-200 dark:border-gray-700">
-                <li className="mb-10 ml-6">
-                    <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-                        <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-                        </svg>
-                    </span>
-                    <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">Zappian, Bhopal 
-                        <span class="hidden lg:block bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3">Currently Working</span>
-                    </h3>
-                    <p className="text-sm text-zinc-200 mb-2">MERN STACK DEVELOPER INTERN</p>
-                    <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Apr 2024 – Present</time>
-                    <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Collaborated with a team on daily website development tasks for a digital marketing product, participating in daily meetings to align on approaches and project goals. Utilized problem-solving skills to address unique challenges as they arose. Engaged in backend development using technologies such as Node.js, Express.js, and MongoDB.</p>
-                </li>
-                <li className="mb-10 ml-6">
-                    <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-                        <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-                        </svg>
-                    </span>
-                    <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">LeopardRuns Innovation & Technology, Bhopal</h3>
-                    <p className="text-sm text-zinc-200 mb-2">SOFTWARE DEVELOPER INTERN</p>
-                    <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Dec 2023 – Mar 2024</time>
-                    <p className="text-base font-normal text-gray-500 dark:text-gray-400">During a 3-month internship in a dynamic learning environment, I gained practical experience in software development, executing assigned tasks with precision and demonstrating a strong commitment to professional growth. I collaborated seamlessly within project teams, effectively applying theoretical knowledge to real-world software development initiatives. My work involved using a variety of languages and frameworks, including JavaScript, React/Redux, Tailwind CSS, and NodeJS.</p>
-                </li>
-            </ol>
+      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Experience
+          </p>
+          <p className="py-6">These are the organizations I have worked with</p>
         </div>
+        <ol className="relative border-l border-gray-200 dark:border-gray-700">
+          <li className="mb-10 ml-6">
+            <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+              <svg
+                className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+              </svg>
+            </span>
+            <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+              Custom Travel Solutions, Pune
+              <span class="hidden lg:block bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3">
+                Currently Working
+              </span>
+            </h3>
+            <p className="text-sm text-zinc-200 mb-2">FULL STACK DEVELOPER</p>
+            <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+              Aug 2024 – Present
+            </time>
+            <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+              Collaborating with team members on developing a travel-related
+              product, primarily focusing on backend development using Node.js,
+              Express.js, GraphQL, PostgreSQL, and various AWS services.
+              Recently, I started working on a new product, where I contribute
+              as a full-stack developer using ReactJS, TypeScript, Node.js, and
+              Express.js. My focus is on enhancing my efficiency as a developer
+              while actively contributing as a team player.
+            </p>
+          </li>
+          <li className="mb-10 ml-6">
+            <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+              <svg
+                className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+              </svg>
+            </span>
+            <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+              Zappian, Bhopal
+            </h3>
+            <p className="text-sm text-zinc-200 mb-2">
+              MERN STACK DEVELOPER INTERN
+            </p>
+            <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+              Apr 2024 – July 2024
+            </time>
+            <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+              Collaborated with a team on daily website development tasks for a
+              digital marketing product, participating in daily meetings to
+              align on approaches and project goals. Utilized problem-solving
+              skills to address unique challenges as they arose. Engaged in
+              backend development using technologies such as Node.js,
+              Express.js, and MongoDB.
+            </p>
+          </li>
+          <li className="mb-10 ml-6">
+            <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+              <svg
+                className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+              </svg>
+            </span>
+            <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+              LeopardRuns Innovation & Technology, Bhopal
+            </h3>
+            <p className="text-sm text-zinc-200 mb-2">
+              SOFTWARE DEVELOPER INTERN
+            </p>
+            <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+              Dec 2023 – Mar 2024
+            </time>
+            <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+              During a 3-month internship in a dynamic learning environment, I
+              gained practical experience in software development, executing
+              assigned tasks with precision and demonstrating a strong
+              commitment to professional growth. I collaborated seamlessly
+              within project teams, effectively applying theoretical knowledge
+              to real-world software development initiatives. My work involved
+              using a variety of languages and frameworks, including JavaScript,
+              React/Redux, Tailwind CSS, and NodeJS.
+            </p>
+          </li>
+        </ol>
+      </div>
     </div>
-    
-    );
+  );
 }
